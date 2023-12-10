d54016e4424a218a8a8f3e6f7d7188eb19c93a32
bbobrek <bartosz.bobrek1993@gmail.com>
2023-12-10 12:59:03 +0100

    Random controller with params

---
+++ src/main/java/pl/coderslab/controller/RandomController.java
@@ -0,0 +1,29 @@
+package pl.coderslab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Random;
+
+@Controller
+public class RandomController {
+
+    private Random random = new Random();
+
+
+    @GetMapping(value = "/random/{min}/{max}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String showRandomMinMax(@PathVariable int min, @PathVariable int max) {
+        int randomNumber = random.nextInt(max - min + 1) + min;
+        return "Użytkownik podał wartość min: " + min + " i max: " + max + ". Wylosowano liczbę: " + randomNumber;
+    }
+
+    @GetMapping(value = "/random/{max}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String showRandomMax(@PathVariable int max) {
+        int randomNumber = random.nextInt(max) + 1;
+        return "Użytkownik podał wartość: " + max + ". Wylosowano liczbę: " + randomNumber;
+    }
+
+}
