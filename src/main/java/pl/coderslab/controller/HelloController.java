40b30bfa571e76c00313b3a99cf9a023adb945f0
bbobrek <bartosz.bobrek1993@gmail.com>
2023-12-10 15:22:02 +0100

    Params, 3 & 4 & 5

--- src/main/java/pl/coderslab/controller/HelloController.java
+++ src/main/java/pl/coderslab/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,6 +28,12 @@ public class HelloController {
             model.addAttribute("backgroundColor", "black");
         }
         return "home";
+    }
+
+    @GetMapping(value = "/hello/{firstName}/{lastName}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String hello(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+        return "Witaj " + firstName + " " + lastName;
     }
 
     @RequestMapping("/hello")
